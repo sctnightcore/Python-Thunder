9f5224cf7bbc9f25830464bdce3d93cdd9a86520
sctnightcore <23263315+sctnightcore@users.noreply.github.com>
2020-10-01 14:36:31 +0700

    Add Correct the Mistakes

---
+++ CorrectTheMistake.py
@@ -0,0 +1,14 @@
+#Problem statement: Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+#Problem Link: https://edabit.com/challenge/uJSWmsoY79Y5cuzxa
+
+
+def squared(n):
+    return n * n
+
+print("squared(0)",squared(0))
+print("squared(1)",squared(1))
+print("squared(2)",squared(2))
+print("squared(3)",squared(3))
+print("squared(7)",squared(7))
+print("squared(9)",squared(9))
+print("squared(15)",squared(15))
